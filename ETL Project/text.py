d0e6effd60ec557a4b93c857ff451586f3f7cd6b
Srikanta Ghosh <srikanta.rony@gmail.com>
2025-01-31 17:03:01 +0530

    dbt airboltic

---
+++ ETL Project/text.py
@@ -0,0 +1,7 @@
+# Run this command
+
+jupyter notebook
+
+## Setting up aws cli- https://www.youtube.com/watch?v=gswVHTrRX8I
+##doc- https://docs.aws.amazon.com/cli/latest/userguide/getting-started-install.html
+
