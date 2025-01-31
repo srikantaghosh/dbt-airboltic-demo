6e6ae01fdee27edffae0e91af8277cbac7e17dec
Srikanta Ghosh <122613833+srikantaghosh@users.noreply.github.com>
2025-01-31 17:12:57 +0530

    Delete ETL Project directory

--- ETL Project/text.py
+++
@@ -1,7 +0,0 @@
-# Run this command
-
-jupyter notebook
-
-## Setting up aws cli- https://www.youtube.com/watch?v=gswVHTrRX8I
-##doc- https://docs.aws.amazon.com/cli/latest/userguide/getting-started-install.html
-
